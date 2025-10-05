28a2c1a69ad299f19b8304c2a2e1993f671db6f4
Freddy Fernando Huanca Irahola <fernandohuanca2005@gmail.com>
2025-10-04 23:15:36 -0400

    Huffman completado

--- codigos.py
+++ codigos.py
@@ -3,38 +3,68 @@ def ficticio(r, vector):
     izq = 0
     alfa = 0
     while izq < q:
-        izq = r + alfa* (r-1)    
-        alfa = alfa+1
-    res = izq - q
-    return res
+        izq = r + alfa * (r - 1)
+        alfa += 1
+    return izq - q
 
-def sumar_r(r, vector):
-    print(vector)
-    while r != len(vector):
-        suma = 0
-        cont = 0
-        while cont != r:
-            suma = suma + vector[0]
-            vector.pop(0)    
-            cont = cont + 1
-        vector.append(round(suma,5))
-        vector.sort()
-        print(vector)
+def sumar_r(r, nodos):
+    while len(nodos) > r:
+        grupo = nodos[:r]
+        suma_prob = sum([n[0] for n in grupo])
+        nuevo_nodo = [round(suma_prob, 5), grupo, '']
+        nodos = nodos[r:]
+        nodos.append(nuevo_nodo)
+        nodos.sort(key=lambda x: x[0])
+    return nodos
+
+def propagar_codigos(nodo, prefijo=''):
+    if isinstance(nodo[1], list):
+        for i, hijo in enumerate(nodo[1]):
+            propagar_codigos(hijo, prefijo + str(i))
+    else:
+        nodo[2] = prefijo
+
+def extraer_simbolos(nodo, lista):
+    if isinstance(nodo[1], list):
+        for hijo in nodo[1]:
+            extraer_simbolos(hijo, lista)
+    else:
+        lista.append(nodo)
 
 r = int(input("Ingrese el numero de la fuente: "))
 vector_fuente = []
-for i in range(1, r+1):
-    m = int(input("Ingrese la fuente de r simbolos "+str(i)+': '))
+for i in range(1, r + 1):
+    m = int(input(f"Ingrese la fuente de r simbolos {i}: "))
     vector_fuente.append(m)
 vector_fuente.sort()
 s = int(input("Ingrese el numero de simbolos: "))
 vector_prob = []
-for i in range(1, s+1):
-    p = float(input("Ingrese la probabilida de S"+str(i)+': '))
+for i in range(1, s + 1):
+    p = float(input(f"Ingrese la probabilida de S{i}: "))
     vector_prob.append(p)
-print("Fuente: ",vector_fuente)
-for i in range(0,ficticio(r,vector_prob)):
+
+for i in range(ficticio(r, vector_prob)):
     vector_prob.append(0)
 vector_prob.sort()
-print("Suma r: ")
-sumar_r(r, vector_prob)
+
+nodos = []
+for i in range(len(vector_prob)):
+    simbolo = f"S{i+1}"
+    prob = vector_prob[i]
+    nodos.append([prob, simbolo, ''])
+
+nodos_finales = sumar_r(r, nodos)
+
+ultimos = nodos_finales[-r:]
+ultimos.sort(key=lambda x: x[0], reverse=True)
+for i, nodo in enumerate(ultimos):
+    propagar_codigos(nodo, str(i))
+
+simbolos_finales = []
+for nodo in nodos_finales:
+    extraer_simbolos(nodo, simbolos_finales)
+
+simbolos_finales.sort(key=lambda x: x[0], reverse=True)
+
+for s in simbolos_finales:
+    print(s)
